935abbca1627db04ab70c97594fcd4b879dfd380
Pedro Jose Manzueta Sepulveda <pedromanzueta017@gmail.com>
2024-06-30 00:02:06 -0400

    added the loop

--- src/main.rs
+++ src/main.rs
@@ -117,7 +117,7 @@ impl Students {
     }
 }
 
-fn show_students(val: VecDeque<Students>) {
+fn show_students(val: &VecDeque<Students>) {
     let mut name = String::new();
     println!("Write nothing if you want all the students, write the name of the student: ");
     stdout().flush().unwrap();
@@ -158,11 +158,53 @@ fn remove_student(val: &mut VecDeque<Students>) {
     }
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(),Errornumber> {
     let mut students_vec: VecDeque<Students> = VecDeque::new();
 
-    let student = Students::default();
-    students_vec.push_front(student);
-    show_students(students_vec);
+    loop {
+        println!("Options:");
+        println!("1. Add student");
+        println!("2. Show students");
+        println!("3. Update student");
+        println!("4. Remove student");
+        println!("5. Exit");
+        let choice = read_input("Choose an option: ")?;
+
+        match choice.as_str() {
+            "1" => {
+                match Students::add_student() {
+                    Ok(student) =>  students_vec.push_back(student),
+                    Err(e) => println!("Error: {}", e),
+                }
+            }
+            "2" => show_students(&students_vec),
+            "3" => {
+                if students_vec.is_empty() {
+                    println!("No students to update.");
+                } else {
+                    let id_input = read_input("Enter the ID of the student to update: ")?;
+                    {if let Ok(id) = id_input.parse::<u32>() {
+                        if let Some(student) = students_vec.iter_mut().find(|s| s.id == id) {
+                            if let Err(e) = Students::update_student(student) {
+                                println!("Error: {}", e);
+                            }
+                        } else {
+                            println!("Student with ID {} not found.", id);
+                        }
+                    } else {
+                        println!("Invalid ID.");
+                    }
+                }}
+            }
+            "4" => remove_student(&mut students_vec),
+            "5" => {
+                println!("Exiting...");
+                break;
+            }
+            _ => println!("Invalid option, please try again."),
+        }
+    }
+
     Ok(())
 }
+
